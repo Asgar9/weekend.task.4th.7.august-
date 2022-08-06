946a3a3c0eb04f2ee19311a1236132beb50efab5
Asgar9 <asgaryas@code.edu.az>
2022-08-07 02:36:39 +0400

    checker of number and index of array

---
+++ finder.index/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace finder.index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   int num= 5; int[] can = { 7, 45, 34, 8, 6 };
+            Console.WriteLine(Finder(num,can));
+        }
+        static bool Finder(int number,int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (number==arr[i])
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+    }
+}
